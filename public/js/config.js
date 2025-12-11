83e377010ecd9a2fa8597ffdb03e276c618974eb
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2025-12-12 02:30:58 +0530

    added

--- public/js/config.js
+++ public/js/config.js
@@ -3,4 +3,4 @@
   Replace placeholders with your Supabase Project URL and ANON key, OR set environment/build script to write this file.
 */
 export const SUPABASE_URL="https://eppknftftmgegebnuhxg.supabase.co"
-export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwcGtuZnRmdG1nZWdlYm51aHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NzM0MzYsImV4cCI6MjA4MTA0OTQzNn0.4C_xn0-MGREQIHJZgTNaII3uEaCabAOU6Qncsk6GSAI";
+export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwcGtuZnRmdG1nZWdlYm51aHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NzM0MzYsImV4cCI6MjA4MTA0OTQzNn0.4C_xn0-MGREQIHJZgTNaII3uEaCabAOU6Qncsk6GSAI"
